64cd6e225acf8a6681e46317f8083415e44a1724
Gabriella  Miesner <ella.miesner@gmail.com>
2021-03-08 04:17:39 +0000

    plus and minus

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,20 @@
+function plusMinus(plus, minus){
+  document.getElementById(plus).style.display = "inline"
+    document.getElementById(minus).style.display = "none"
+}
+
 function showDataScience(){
     div = document.getElementById('data_sci');
     if (div.style.display == "block"){
       div.style.display = "none";
+      plusMinus('data_sci_plus', 'data_sci_minus')
     }else{
     div.style.display = "block";
     document.getElementById('vr').style.display = "none";
     document.getElementById('web_dev').style.display = "none"
     document.getElementById('interview').style.display = "none"
     document.getElementById('game_dev').style.display = "none"
+    plusMinus('data_sci_minus', 'data_sci_plus')
     }
 }
 
@@ -15,12 +22,14 @@ function showVirt(){
     div = document.getElementById('vr');
     if (div.style.display == "block"){
       div.style.display = "none";
+      plusMinus('vr_plus', 'vr_minus')
     }else{
     div.style.display = "block";
     document.getElementById('data_sci').style.display = "none";
     document.getElementById('web_dev').style.display = "none"
     document.getElementById('interview').style.display = "none"
     document.getElementById('game_dev').style.display = "none"
+   plusMinus('vr_minus', 'vr_plus')
     }
 }
 
@@ -28,12 +37,14 @@ function showWebDev(){
     div = document.getElementById('web_dev');
     if (div.style.display == "block"){
       div.style.display = "none";
+      plusMinus('web_dev_plus','web_dev_minus')
     }else{
     div.style.display = "block";
     document.getElementById('vr').style.display = "none";
     document.getElementById('data_sci').style.display = "none"
     document.getElementById('interview').style.display = "none"
     document.getElementById('game_dev').style.display = "none"
+    plusMinus('web_dev_minus', 'web_dev_plus')
     }
 }
 
@@ -41,12 +52,14 @@ function showGameDev(){
     div = document.getElementById('game_dev');
     if (div.style.display == "block"){
       div.style.display = "none";
+     plusMinus('game_dev_plus', 'game_dev_minus')
     }else{
     div.style.display = "block";
     document.getElementById('vr').style.display = "none";
     document.getElementById('web_dev').style.display = "none"
     document.getElementById('interview').style.display = "none"
     document.getElementById('data_sci').style.display = "none"
+    plusMinus('game_dev_minus', 'game_dev_plus')
     }
 }
 
@@ -54,11 +67,13 @@ function showInterview(){
     div = document.getElementById('interview');
     if (div.style.display == "block"){
       div.style.display = "none";
+     plusMinus('interview_plus', 'interview_minus')
     }else{
     div.style.display = "block";
     document.getElementById('vr').style.display = "none";
     document.getElementById('web_dev').style.display = "none"
     document.getElementById('data_sci').style.display = "none"
     document.getElementById('game_dev').style.display = "none"
+    plusMinus('interview_minus', 'interview_plus')
     }
 }
